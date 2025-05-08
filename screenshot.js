ec0caf0502779c8f8d92336ca0e541477df15d79
Francis Wayungi <FrancisW@ithubauganda.co.ug>
2025-05-08 05:09:21 +0300

    ADD: full script to auto capture screenshots

--- screenshot.js
+++ screenshot.js
@@ -1,12 +1,23 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const url = 'https://oms-weekly-display.paytronix.co.ug/'; 
-  const browser = await puppeteer.launch();
+  const url = 'https://oms-weekly-display.paytronix.co.ug';
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+  });
   const page = await browser.newPage();
-  await page.goto(url, { waitUntil: 'networkidle2' });
-  await page.screenshot({ path: './screenshots/oms-weekly-display/screenshot.png', fullPage: true });
-  await browser.close();
+  await page.setViewport({ width: 1280, height: 800 });
+  await page.goto(url, { waitUntil: 'networkidle0' });
+ 
+  while (true) {
+    await new Promise(r => setTimeout(r, 14000)); /***  wait 14 seconds to ensure complete loading ***/
+    await page.screenshot({ path: `screenshots/screenshot.png`, fullPage: true });
+    console.log(`Captured screenshot.png`);
+  }
+
+  // await browser.close();
 })();
+
 
 
